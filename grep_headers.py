44fb4b5c0adc4b1d07954a9ade0313a36a724ffa
Igor C <igor.bioinfo@gmail.com>
2018-05-09 22:32:40 -0400

    Added argparse and a bit of documentation to grep_headers.py

--- grep_headers.py
+++ grep_headers.py
@@ -3,15 +3,36 @@
 #grep_headers.py
 #04/2018
 
-
+'''grep_species -s species_folder -i headers_file -r refprot_folder
+this program finds all sequences of a given protein in refseq for all species in a list 
+'''
 __author__ = 'Igor Rodrigues da Costa'
 __contact__ = 'igor.bioinfo@gmail.com'
 
 import os
+import argparse
 from subprocess import Popen
 
-def get_species():
-    species_folder = '/home/igor/ancestral/species/'
+def argument_parser(hlp = False):
+    '''grep_headers.py'''
+
+    parser = argparse.ArgumentParser(description = 'grep_headers.py',\
+                                     argument_default = None, fromfile_prefix_chars = '@')
+    parser.add_argument('-s', '--species', nargs = '?', type = str, required = True,\
+                        dest = 'species', help = 'Path to the species folder.')
+    parser.add_argument('-i', '--headers', nargs = '?', type = str, required = True,\
+                        dest = 'headers', help = 'Path to the headers file.')
+    parser.add_argument('-r', '--refprot', nargs = '?', type = str, required = True,\
+                        dest = 'refprot', help = 'Path to the refprot folder.')
+    parser.add_argument('-o', '--outpath', nargs = '?', type = str, default = os.getcwd(),\
+                        dest = 'outpath', help = 'Path where the aligned results will be saved. (default: %(default)s)')
+    if hlp:
+        args = parser.parse_args(['-h'])
+    else:
+        args = parser.parse_args().__dict__
+    return args
+
+def get_species(species_folder = '/home/igor/ancestral/species/'):
     files = os.listdir(species_folder)
     species = {}
     for f in files:
@@ -34,12 +55,15 @@ def run_grep(species, header_file = '/home/igor/ancestral/L3_headers2'):
                 found.append(l.strip())
     return '\n'.join(found)
 
-def main():
+def main(args):
     '''this should get the species, order by kingdom, then search headers of this species in
      the header file, assuring there is one seq for sp. Find the sequence in the refprot file, concatenate and align.'''
-    species = get_species()
+    species = get_species(args['species'])
     for k in species:
         with open(k+'_headers2', 'w') as k_header: #headers of each kingdom
-            headers = run_grep(species[k])
+            headers = run_grep(args['headers'], species[k])
             k_header.write(headers)
-main()
+
+if __name__ == '__main__':
+    args = argument_parser()
+    main(args)   
